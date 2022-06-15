58eafed48ac55f1e17391a9d8b0ab02a2584ec58
YdrMaster <ydrml@hotmail.com>
2022-06-15 14:13:59 +0800

    feat(example): 现在示例是一个串口助手

--- src/bin/example.rs
+++ src/bin/example.rs
@@ -1,9 +1,21 @@
 ﻿use serial_port::{Port, SerialPort};
 
 fn main() {
-    let port = Port::open(&4, 115200, u32::MAX).unwrap();
-    let mut buf = [0u8; 256];
+    use std::{sync::Arc, thread};
+    let port = Port::list().into_iter().next().expect("no serial port");
+    let port = Arc::new(Port::open(&port.key, 115200, u32::MAX).unwrap());
+    {
+        let port = port.clone();
+        thread::spawn(move || {
+            let mut line = String::new();
+            while std::io::stdin().read_line(&mut line).is_ok() {
+                port.write(line.as_bytes());
+                line.clear();
+            }
+        });
+    }
+    let mut buf = [0u8; 1024];
     while let Some(n) = port.read(&mut buf) {
-        println!("{:?}", std::str::from_utf8(&buf[..n]));
+        println!("{}", std::str::from_utf8(&buf[..n]).unwrap());
     }
 }
